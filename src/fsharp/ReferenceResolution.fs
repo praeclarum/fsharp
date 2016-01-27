c31320a130d339fa2633d25430bfe8de2068a4f2
Frank A. Krueger <fak@praeclarum.org>
2016-01-27 11:06:28 -0800

    OK, I mean it this time: fix the Xamarin bug

--- src/fsharp/ReferenceResolution.fs
+++ src/fsharp/ReferenceResolution.fs
@@ -7,7 +7,7 @@ type ILAssemblyTypeForwarderComparer () =
         member this.Compare (x, y) =
             if x = y then 0
             else if fst x < fst y then -1
-            else if snd x < snd y then -1
+            else if fst x = fst y && snd x < snd y then -1
             else 1
 
 module internal MSBuildResolver = 
